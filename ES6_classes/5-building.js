6600fae26cad136249df4bec741c73c61d5d5501
ChSPN <charlene.scomparin@gmail.com>
2024-07-18 15:22:14 +0200

    fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,19 +1,22 @@
+// 5-building.js
 class Building {
-	constructor(sqft) {
-		if (new.target === Building) {
-			throw new TypeError('Cannot construct Building instances directly');
-		}
-		if (this.evacuationWarningMessage === undefined) {
-			throw new TypeError('Class extending Building must override evacuationWarningMessage');
-		}
-		this._sqft = sqft;
-	}
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building');
+    }
+  }
 
-	get sqft() {
-		return this._sqft;
-	}
+  get sqft() {
+    return this._sqft;
+  }
 
-	evacuationWarningMessage() {
-		throw new Error('You need to implement evacuationWarningMessage in your class');
-	}
+  // Désactivation de la règle ESLint pour cette méthode spécifique
+  /* eslint-disable class-methods-use-this */
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+  /* eslint-enable class-methods-use-this */
 }
+
+export default Building;
