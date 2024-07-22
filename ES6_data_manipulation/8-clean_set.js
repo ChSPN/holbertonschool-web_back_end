b0a32b089d828a8aa739b31ac687204f6410c75f
ChSPN <charlene.scomparin@gmail.com>
2024-07-22 16:36:33 +0200

    8. Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+// 8-clean_set.js
+function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  const result = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
+
+export default cleanSet;
