d0e43c8e0c9ba7403db74cded8170d9b08973f03
ChSPN <charlene.scomparin@gmail.com>
2024-07-22 17:27:40 +0200

    fix the code

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
+// 2-get_students_by_loc.js
 function getStudentsByLocation(students, city) {
   return students.filter((student) => student.location === city);
 }
 
-// Exemple d'utilisation de la fonction pour éviter l'erreur "no-unused-vars"
-console.log(getStudentsByLocation([{ id: 1, firstName: 'Guillaume', location: 'San Francisco' }], 'San Francisco'));
+export default getStudentsByLocation;
