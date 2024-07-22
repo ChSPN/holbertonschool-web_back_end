5b61a70ce61b726d2e8abde9895feac12e036e87
ChSPN <charlene.scomparin@gmail.com>
2024-07-22 11:17:05 +0200

    1. More mapping

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,9 @@
 function getListStudentIds(array) {
   if (!Array.isArray(array)) {
-	return [];
+    return [];
   }
-  return array.map(student => student.id);
+  return array.map((student) => student.id);
 }
+
+// Exemple d'utilisation de la fonction pour éviter l'erreur "no-unused-vars"
+console.log(getListStudentIds([{ id: 1 }, { id: 2 }, { id: 3 }]));
