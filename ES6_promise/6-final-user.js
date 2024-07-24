8ddf606457542d1fb13d51fc92b1e170efa25a4c
ChSPN <charlene.scomparin@gmail.com>
2024-07-24 09:58:23 +0200

    fix 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,20 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([signUpUser(firstName, lastName),
+    uploadPhoto(fileName)]);
+
+  return results.map((result) => {
     if (result.status === 'fulfilled') {
-      return {
-        status: 'fulfilled',
-        value: result.value,
-      };
+      return { status: 'fulfilled', value: result.value };
     }
-    return {
-      status: 'rejected',
-      value: result.reason,
-    };
-  }));
+    return { status: 'rejected', value: result.reason.toString() };
+  });
 }
