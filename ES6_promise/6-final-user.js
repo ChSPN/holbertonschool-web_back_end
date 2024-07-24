85983b87dc25a8e8e36a329822772db0c03d83be
ChSPN <charlene.scomparin@gmail.com>
2024-07-24 09:41:14 +0200

    6. Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,20 +4,17 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: 'fulfilled',
-          value: result.value,
-        };
-      } else {
-        return {
-          status: 'rejected',
-          value: result.reason,
-        };
-      }
-    });
-  });
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: 'fulfilled',
+        value: result.value,
+      };
+    }
+    return {
+      status: 'rejected',
+      value: result.reason,
+    };
+  }));
 }
