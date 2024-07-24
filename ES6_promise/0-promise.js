cb92c43df7a77310618ecdcf7c5902836800fc38
ChSPN <charlene.scomparin@gmail.com>
2024-07-24 16:11:47 +0200

    fix 0

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,5 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve) => {
-    // To simulate asynchronous behavior:
-    // Use setTimeout to mimic an API call
-    setTimeout(() => {
-      // Resolve the promise with a sample response
-      resolve('Response from API');
-    }, 1000); // 1 second delay
+    resolve('API response');
   });
 }
