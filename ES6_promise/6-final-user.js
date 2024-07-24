91f7c265f975c00cdd3c223a096cbde48265df97
ChSPN <charlene.scomparin@gmail.com>
2024-07-24 09:37:31 +0200

    fix 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,3 +1,23 @@
-export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName });
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      } else {
+        return {
+          status: 'rejected',
+          value: result.reason,
+        };
+      }
+    });
+  });
 }
