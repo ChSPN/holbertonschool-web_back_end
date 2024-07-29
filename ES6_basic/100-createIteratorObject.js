0bd7faf24ba0c45877766854bcbfd9ac612a1a93
ChSPN <charlene.scomparin@gmail.com>
2024-07-29 10:50:20 +0200

    fix advanced task

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,20 +1,9 @@
 export default function createIteratorObject(report) {
-  // Stocker tous les employés de tous les départements dans un tableau unique
   const allEmployees = [];
-  for (const department of Object.keys(report.allEmployees)) {
-    allEmployees.push(...report.allEmployees[department]);
+
+  for (const department of Object.values(report.allEmployees)) {
+    allEmployees.push(...department);
   }
 
-  // Créer et retourner l'itérateur
-  let currentIndex = 0;
-  return {
-    next() {
-      if (currentIndex < allEmployees.length) {
-        const value = allEmployees[currentIndex];
-        currentIndex += 1;
-        return { value, done: false };
-      }
-      return { done: true };
-    },
-  };
+  return allEmployees[Symbol.iterator]();
 }
