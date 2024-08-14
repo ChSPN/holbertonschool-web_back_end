1e7dcb8022a976d96bfbd690e228ac2ac0874dd4
ChSPN <charlene.scomparin@gmail.com>
2024-08-14 09:39:42 +0200

    add comments

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""function that measures the total execution time for wait_n(n, max_delay)"""
+
 import asyncio
 import random
 import time
@@ -8,6 +10,8 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    """function that measures the total execution
+    time for wait_n(n, max_delay)"""
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
