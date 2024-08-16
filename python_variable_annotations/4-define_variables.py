ecdec991fc393bbe84e463b588b4356d19657339
ChSPN <charlene.scomparin@gmail.com>
2024-08-16 21:29:18 +0200

    fix comment 4-define_variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""commentary"""
+
 a: int = 1
+"""commentary"""
 pi: float = 3.14
+"""commentary"""
 i_understand_annotations: bool = True
+"""commentary"""
 school: str = "Holberton"
+"""commentary"""
