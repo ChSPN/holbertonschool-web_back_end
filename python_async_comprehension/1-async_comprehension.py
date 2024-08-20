cc3cb8ea5489af1f605df3eefd99a75289630c06
ChSPN <charlene.scomparin@gmail.com>
2024-08-20 11:15:26 +0200

    1. Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,7 @@ using async comprehensions.
 """
 
 from typing import List
-from 0-async_generator import async_generator
+from 0-async_generator import async_generator  # Correction de l'importation
 
 
 async def async_comprehension() -> List[float]:
@@ -16,4 +16,5 @@ async def async_comprehension() -> List[float]:
     Returns:
         List[float]: A list of 10 random floating-point numbers.
     """
-    return [number async for number in async_generator()]
+    return [number async for number in async_generator()][:10]
+# Limite à 10 nombres
