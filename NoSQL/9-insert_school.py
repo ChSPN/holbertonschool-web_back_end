45851217f9f2b4dd2a73d7ddfaad7b20e2e6c893
ChSPN <charlene.scomparin@gmail.com>
2024-08-22 14:22:46 +0200

    fix without one

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -26,5 +26,3 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     school_collection = client.my_db.school
 
-    # Insertion d'une nouvelle école pour tester
-    new_school_id = insert_school(school_collection, name="UCSF", address="505 P
