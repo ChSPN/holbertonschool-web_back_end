be6331bbf369bf97ecaccfa3dd3ecfa6814e9bc5
ChSPN <charlene.scomparin@gmail.com>
2024-08-22 14:51:29 +0200

    fix

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
-
-
 def update_topics(mongo_collection, name, topics):
     """
     Changes all topics of a school document based on the name.
